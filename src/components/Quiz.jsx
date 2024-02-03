9346d380c2b82caa0efbead6b00b17992b34e099
alekseypetrenko <menefrego5@gmail.com>
2024-02-03 17:11:39 +0200

    quizIsComplete

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCompletedImage from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -10,12 +11,26 @@ export default function Quiz() {
     setUserAnswers((prevAnswer) => [...prevAnswer, answer]);
   }
 
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompletedImage} alt="Quiz completed" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffleAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffleAnswers.sort((a, b) => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div className="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => {
+          {shuffleAnswers.map((answer) => {
             return (
               <li key={answer} className="answer">
                 <button onClick={() => handleSelectAnswer(answer)}>
